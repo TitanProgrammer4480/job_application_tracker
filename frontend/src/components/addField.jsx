7a53744507afb26649f81759aeec12348590f23e
TitanProgrammer4480 <sebi-2007@web.de>
2025-03-13 16:30:19 +0100

    finished the app

--- frontend/src/components/addField.jsx
+++ frontend/src/components/addField.jsx
@@ -47,41 +47,29 @@ function MainField() {
 
   return (
     <div className="w-full h-[85vh] mt-20 flex flex-col items-center">
-      <form onSubmit={handleSubmit}>
-        <div className='flex flex-col gap-2'>
-          <div className='flex flex-row gap-2'>
-            <label className="floating-label">
-              <input type="text" placeholder="Job name" name="jobName" required className="input input-sm" />
-              <span>Job name:</span>
-            </label>
-            <label className="floating-label">
-              <input type="text" placeholder="Company name" name="companyName" required className="input input-sm" />
-              <span>Company name:</span>
-            </label>
-            <label className="floating-label">
-                <input type="text" placeholder="Place" name="place" required className="input input-sm" />
-                <span>Place:</span>
-            </label>
-            <label className="floating-label">
-              <input type="text" placeholder="Link" name="link" required className="input input-sm" />
-              <span>Link:</span>
-            </label>
+      <form onSubmit={handleSubmit} className='w-[95%]'>
+        <fieldset className='fieldset bg-base-200 border border-base-300 p-6 rounded-box'>
+          <div className='join gap-2'>
+            <label className="fieldset-label">Job name:</label>
+            <input type="text" placeholder="Job name" name="jobName" required className="input input-sm" />
+            <label className="fieldset-label">Company name:</label>
+            <input type="text" placeholder="Company name" name="companyName" required className="input input-sm" />
+            <label className="fieldset-label">Place:</label>
+            <input type="text" placeholder="Place" name="place" required className="input input-sm" />
+            <label className="fieldset-label">Link:</label>
+            <input type="text" placeholder="Link" name="link" required className="input input-sm" />
           </div>
-          <div className='flex flex-row gap-2'>
-            <label className="label">
-              <span className='pr-2'>Applied:</span>
-              <input type="checkbox" name="applied" className="checkbox" />
-            </label>
-            <label className="label">
-              <span className='pr-2'>Remote:</span>
-              <input type="checkbox" name="remote" className="checkbox" />
-            </label>
+          <div className='join gap-2 mt-2'>
+            <label className="fieldset-label">Applied:</label>
+            <input type="checkbox" name="applied" className="checkbox" />
+            <label className="fieldset-label">Remote:</label>
+            <input type="checkbox" name="remote" className="checkbox" />
           </div>
-        </div>
-        <button type="submit" className='btn mt-2'>Submit</button>
+          <button type="submit" className='btn mt-2 w-30'>Submit</button>
+        </fieldset>
       </form>
       <div className='mt-20'>
-        <div className="overflow-x-auto">
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
           <table className="table table-sm table-pin-rows">
             <thead>
               <tr>
